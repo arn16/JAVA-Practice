d063e8e9f7b8bfe69dc0888ee2779a39c074e189
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-06 02:42:45 +0530

    Add files via upload

---
+++ Lecture5Code1.java
@@ -0,0 +1,19 @@
+public class Lecture5Code1 {
+    public static void main(String[] args) {
+        // print the pattern 
+        /*
+                    * * * * *
+                    * * * * * 
+                    * * * * * 
+                    * * * * * 
+         */
+
+        int i=1;
+        for(i=1; i<=4; i++){
+        for(int j=1;j<=5;j++){
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+       }   
+    }
+}

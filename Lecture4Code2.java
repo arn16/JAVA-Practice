e3244ab27cf6db924835d87e3d90cd931c832494
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-05 22:59:30 +0530

    Add files via upload

---
+++ Lecture4Code2.java
@@ -0,0 +1,11 @@
+public class Lecture4Code2 {
+    public static void main(String[] args) {
+        // For loop to print numbers from 1 to 20
+
+        for(int a=1; a<=20; a++){
+            System.out.println(a);
+        }
+
+    }
+    
+}

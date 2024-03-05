e673e397327d74351cd231bee69102c0567ba40d
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-06 00:12:51 +0530

    Add files via upload

---
+++ Lecture4Code3.java
@@ -0,0 +1,11 @@
+public class Lecture4Code3 {
+    public static void main(String[] args) {
+        //WHILE LOOP 
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}

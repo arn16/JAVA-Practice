7c19552818fcfbd3cdad68f494173804014d083d
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-05 22:59:16 +0530

    Add files via upload

---
+++ Lecture4Code1.java
@@ -0,0 +1,10 @@
+public class Lecture4Code1 {
+    public static void main(String[] args){
+        // FOR LOOP -> for(initialisation; condition; updation)
+
+        for(int counter = 0; counter<10; counter++){
+            System.out.println("Hello world "+ counter + " times");
+        }
+    }
+    
+}

b3da5c474de80602a1fed9090587304b8b57fc47
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-06 02:44:28 +0530

    Add files via upload

---
+++ Lecture5Code8.java
@@ -0,0 +1,30 @@
+public class Lecture5Code8 {
+    public static void main(String[] args) {
+        // to print the pattern
+        /*
+
+          1
+          2 3
+          4 5 6
+          7 8 9 10
+          11 12 13 14 15
+         
+         */
+         
+        int n=5;
+        int num=1;
+
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+num+" ");
+                num++;
+
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}

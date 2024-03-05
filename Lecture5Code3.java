edbfee042fecd9182957a4a4d328f3e0d2e624ab
arn16 <93872968+arn16@users.noreply.github.com>
2024-03-06 02:43:13 +0530

    Add files via upload

---
+++ Lecture5Code3.java
@@ -0,0 +1,23 @@
+public class Lecture5Code3 {
+    public static void main(String[] args) {
+        // to print the triangle
+        /*
+         
+         * 
+         * *
+         * * *
+         * * * *
+            
+         */
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+               
+            } 
+            System.out.print("\n");
+        }
+
+    }
+    
+}
